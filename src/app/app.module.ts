e8e588c2f1a9710b4d1957cf175e4fe9365d2685
Worawut Keawlomsap <worawut.k@codium.co>
2017-08-18 09:40:21 +0700

    add grid and button for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {routing} from './app.routes';
 import {HandsontableComponent} from "./handsontable/handsontable.module";
+import {AgGridModule} from "ag-grid-angular";
+import {ArgridComponent} from "./ar-grid/ar-grid.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HandsontableComponent
+    HandsontableComponent,
+    ArgridComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    AgGridModule.withComponents([
+      ArgridComponent
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import {RouterModule, Routes} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import {HandsontableComponent} from "./handsontable/handsontable.module";
+import {ArgridComponent} from "./ar-grid/ar-grid.module";
 
 const appRoutes: Routes = [
   {
     path: 'handsontable',
     component: HandsontableComponent
-  }, {path: '**', redirectTo: 'handsontable'}
+  },
+  {
+    path: 'argrid',
+    component: ArgridComponent
+  },
+  {path: '**', redirectTo: 'handsontable'}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
